4300c98b1d8c0a42274388accb4011aa44c3a158
Bruno & Gabriel <fernando.olea76@gmail.com>
2016-02-17 16:34:44 -0200

    Added client model to project

---
+++ spec/factories/clients.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :client do
+    name "MyString"
+    email "MyString"
+    phone "MyString"
+    address "MyString"
+    cpf_cnpj "MyString"
+    contact "MyString"
+  end
+end
