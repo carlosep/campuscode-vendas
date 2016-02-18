c1bb8e3021686283bcf05317db73181009e8b2d2
Jessica e Carlos <carlos_jessica@campuscode.com.br>
2016-02-18 17:32:43 -0200

    Admin validation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,38 +1,39 @@
 class OrdersController < ApplicationController
   before_action :set_collections, only: [:new, :create, :edit]
   before_action :set_order, only: [:show, :edit, :update]
-    def show
-    end
-
-    def new
-      @order = Order.new
-    end
-
-    def create
-      @order = Order.create(order_params)
-      respond_with @order
-    end
-
-    def edit
-    end
-
-    def update
-      @order.update(order_params)
-      respond_with @order
-    end
-
-    private
-
-    def set_collections
-      @customer = Customer.all
-    end
-
-    def set_order
-      @order = Order.find(params[:id])
-    end
-
-    def order_params
-      params.require(:order)
-            .permit(:status, :product, :customer_id, :user_id)
-    end
+  before_action :user_admin, only: [:edit, :update]
+  def show
   end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = current_user.orders.create(order_params)
+    respond_with @order
+  end
+
+  def edit
+  end
+
+  def update
+    @order.update(order_params)
+    respond_with @order
+  end
+
+  private
+
+  def set_collections
+    @customer = Customer.all
+  end
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order)
+          .permit(:status, :product, :customer_id, :user_id)
+  end
+end
