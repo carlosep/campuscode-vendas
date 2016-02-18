d9af17e0a8bfa8f6be24579a14441d7b75517c0f
Fabio e Guilherme <timelocaweb8@campuscode.com.br>
2016-02-18 17:41:40 -0200

    Some minor changes on user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: :show
+  before_action :user_admin
+
   def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    @user.password = 'campuscode'
+    @user.save
+    respond_with @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user)
+          .permit(:name, :email, :password)
+  end
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
