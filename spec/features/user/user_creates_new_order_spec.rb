b9213a6bcab38fab3445e58568048bfcfde5ba47
Carlos E. Palha <carlao_epalha@hotmail.com>
2016-02-19 14:11:14 -0200

    fixed default status on form

--- spec/features/user/user_creates_new_order_spec.rb
+++ spec/features/user/user_creates_new_order_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-# Adicionar User No futuro. na view tbm, motherfucker.
 describe 'User creates new order' do
   scenario 'successfully' do
     order = build(:order)
@@ -10,6 +9,24 @@ describe 'User creates new order' do
     fill_in "Product", with: order.product
     select order.customer.name, from: "Customer"
     select order.status, from: "Status"
+
+    click_on "Create Order"
+
+    expect(page).to have_content "Pedido #{order.id}"
+    expect(page).to have_content order.created_at
+    expect(page).to have_content order.status
+    expect(page).to have_content order.product
+    expect(page).to have_content order.customer.name
+    expect(page).to have_content order.user.name
+  end
+  scenario 'with default scenario' do
+    order = build(:order)
+
+    login
+    visit new_order_path
+
+    fill_in "Product", with: order.product
+    select order.customer.name, from: "Customer"
 
     click_on "Create Order"
 
