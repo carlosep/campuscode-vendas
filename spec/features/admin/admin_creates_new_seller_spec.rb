80d9decd2417b0a995c87acbea37cb96addc82e7
Fábio Goródscy <fabiogorodscy@hotmail.com>
2016-02-19 13:51:09 -0200

    Added test for admin create seleer

---
+++ spec/features/admin/admin_creates_new_seller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe 'Admin creates a new seller' do
+  scenario 'successfully' do
+    user = build(:user)
+    admin = create(:user, is_admin: true)
+
+    login admin
+    visit new_user_path
+
+    fill_in "user[name]", with: user.name
+    fill_in "user[email]", with: user.email
+
+    click_on "Cadastrar Vendedor"
+
+    expect(page).to have_content user.name
+    expect(page).to have_content user.email
+  end
+
+  scenario 'unsuccessfully' do
+    user = build(:user)
+
+    login
+    visit new_user_path
+
+    expect(page).to have_content "Você não é o administrador"
+    expect(current_path).to eq root_path
+  end
+end
