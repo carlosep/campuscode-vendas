8d06cbd2f5581eececd11d7bb1d32db0fab21a95
Jessica e Carlos <carlos_jessica@campuscode.com.br>
2016-02-22 14:51:34 -0300

    Removed comment

--- spec/features/user/user_creates_new_order_spec.rb
+++ spec/features/user/user_creates_new_order_spec.rb
@@ -34,7 +34,7 @@ describe 'User creates new order' do
 
     expect(page).to have_content "Pedido #{order.id}"
     expect(page).to have_content order.created_at
-    expect(page).to have_content order.status #ERROR
+    expect(page).to have_content order.status
     expect(page).to have_content order.product
     expect(page).to have_content order.customer.name
     expect(page).to have_content order.user.name
