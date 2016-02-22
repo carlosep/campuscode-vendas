773d97b751f9938304915ba900c467c5f35f6dee
Bruno and Gabriel <pair@locaweb2.com>
2016-02-22 16:50:33 -0300

    User searchs customer by CPF/CNPJ

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   resources :orders, only: [:new, :create, :show, :edit, :update]
-  resources :customers, only: [:new, :create, :show, :edit, :update]
+  resources :customers, only: [:new, :create, :show, :edit, :update] do
+    collection do
+      get 'search'
+    end
+  end
+
   resources :users, except: [:destroy]
 end
