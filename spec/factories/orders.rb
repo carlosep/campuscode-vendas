c94f3183f51171df5b1d0b9d33a8b92cb4bccaf3
Fabio e Guilherme <timelocaweb8@campuscode.com.br>
2016-02-24 17:03:13 -0300

    Working

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :order do
     status "Aberto"
     product_id 1
+    plan_id 1
     customer
     user
   end
--- spec/features/user/user_creates_new_order_spec.rb
+++ spec/features/user/user_creates_new_order_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 describe 'User creates new order', :js => true do
+  self.use_transactional_fixtures = false
   scenario 'successfully' do
-    order = build(:order)
-    
+    User.last.delete if User.last
+    Customer.last.delete if Customer.last
+    Order.last.delete if Order.last
+    user = create(:user, email: 'a@gmail.com', password: '12345678')
+    customer = create(:customer, email: 'a@gmail.com')
+    order = build(:order, user: user, customer: customer)
+
+    visit root_path
+
+    fill_in 'user[email]',    with: 'a@gmail.com'
+    fill_in 'user[password]', with: '12345678'
+
+    click_on 'Log in'
+
     visit new_order_path
 
     select 'Hospedagem', from: 'order[product_id]'
@@ -20,5 +33,9 @@ describe 'User creates new order', :js => true do
     expect(page).to have_content 'Hospedagem'
     expect(page).to have_content order.customer.name
     expect(page).to have_content order.user.name
+
+    User.last.delete
+    Customer.last.delete
+    Order.last.delete
   end
 end
