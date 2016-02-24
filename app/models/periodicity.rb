1aed3dd25ae5612ea083b9e78bc78663ad046a1d
Bruno and Gabriel <pair@locaweb2.com>
2016-02-24 16:05:48 -0300

    Coupon can be used on order

--- app/models/periodicity.rb
+++ app/models/periodicity.rb
@@ -1,4 +1,5 @@
 require 'active_resource'
+
 class Periodicity < ActiveResource::Base
-  self.site = "http://localhost:3001"
+  self.site = 'http://localhost:3001'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160224154257) do
+ActiveRecord::Schema.define(version: 20160224185253) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160224154257) do
     t.integer  "user_id"
     t.integer  "customer_id"
     t.integer  "product_id"
+    t.string   "coupon"
   end
 
   add_index "orders", ["customer_id"], name: "index_orders_on_customer_id"
