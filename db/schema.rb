9e861a5556a3dee9b6ef6386c9c70dc46ebea67f
Fabio e Guilherme <timelocaweb8@campuscode.com.br>
2016-02-24 13:49:12 -0300

    Removing default selected boxes and tests from orders view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223205325) do
+ActiveRecord::Schema.define(version: 20160224154257) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20160223205325) do
     t.string   "contact_name"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.date     "birthdate"
     t.string   "company_name"
     t.date     "birth_date"
   end
--- spec/features/user/user_creates_new_order_spec.rb
+++ spec/features/user/user_creates_new_order_spec.rb
@@ -21,24 +21,4 @@ describe 'User creates new order' do
     expect(page).to have_content order.customer.name
     expect(page).to have_content order.user.name
   end
-
-  scenario 'with default status and customer' do
-    order = build(:order)
-
-    login
-    visit new_order_path
-
-    select 'Hospedagem', from: 'Product'
-
-    within ('section#order_form') do
-      click_on "Create Order"
-    end
-
-    expect(page).to have_content "Order #{order.id}"
-    expect(page).to have_content order.created_at
-    expect(page).to have_content order.status
-    expect(page).to have_content 'Hospedagem'
-    expect(page).to have_content order.customer.name
-    expect(page).to have_content order.user.name
-  end
 end
